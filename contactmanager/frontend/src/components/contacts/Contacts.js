8d199234af3407607f9ffb391ef20453b157e73d
Tasfia Islam <tasf1a027@gmail.com>
2020-11-16 19:50:39 +0600

    Implemented Redux

--- contactmanager/frontend/src/components/contacts/Contacts.js
+++ contactmanager/frontend/src/components/contacts/Contacts.js
@@ -1,6 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {getContacts} from '../../actions/contacts';
 
 export class Contacts extends Component {
+    static PropTypes =  {
+        contacts: PropTypes.array.isRequired
+    }
+    componentDidMount(){
+        this.props.getContacts();
+    }
     render() {
         return (
             <div>
@@ -10,4 +19,8 @@ export class Contacts extends Component {
     }
 }
 
-export default Contacts
+const mapStateToProps = state => ({
+    contacts: state.contacts.contacts
+});
+
+export default connect(mapStateToProps, {getContacts})(Contacts);
