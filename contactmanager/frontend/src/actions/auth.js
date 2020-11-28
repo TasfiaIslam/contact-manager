1c9ebf1fa8f28c974c76600597bb41cf167f2c50
Tasfia Islam <tasf1a027@gmail.com>
2020-11-29 02:04:06 +0600

    Login authentication

--- contactmanager/frontend/src/actions/auth.js
+++ contactmanager/frontend/src/actions/auth.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 import { returnErrors } from "./messages";
-import { USER_LOADING, USER_LOADED, AUTH_ERROR } from "./types";
+import {
+  USER_LOADING,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+} from "./types";
 
 // CHECK TOKEN AND LOAD USER
 export const loadUser = () => (dispatch, getState) => {
@@ -34,6 +40,39 @@ export const loadUser = () => (dispatch, getState) => {
       dispatch(returnErrors(err.response.data, err.response.status));
       dispatch({
         type: AUTH_ERROR,
+      });
+    });
+};
+
+// LOGIN USER
+export const login = (username, password) => (dispatch) => {
+  //Headers
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+    },
+  };
+
+  // Request Body
+  //const body = JSON.stringify({ username, password });
+
+  const auth = {
+    username: username,
+    password: password,
+  };
+
+  axios
+    .post("/api/auth/login", auth, config)
+    .then((res) => {
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+    })
+    .catch((err) => {
+      dispatch(returnErrors(err.response.data, err.response.status));
+      dispatch({
+        type: LOGIN_FAIL,
       });
     });
 };
